cd89668295192ec704de7daa72d28089505e837a
Sxy <82423990@qq.com>
2017-08-20 22:18:42 +0800

    模型的编写和迁移

--- dashborad/models.py
+++ dashborad/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Server(models.Model):
+    hostname = models.CharField(max_length=32, unique=True)
+    ip = models.CharField(max_length=15, unique=True)
+    cpu = models.CharField(max_length=50, null=True)
+    mem = models.CharField(max_length=50)
+    disk = models.CharField(max_length=50)
+    sn = models.CharField(max_length=60)
+    idc = models.CharField(max_length=50)
+    ipinfo = models.CharField("[{'eth0': '192.168.168.1', 'mac_addr': 'dsfsd'}]", max_length=50)
+    product = models.CharField(max_length=50)
+    remark = models.TextField(default='')
+    class Meta:
+        db_table = 'server'
+
